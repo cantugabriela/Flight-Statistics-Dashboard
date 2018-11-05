1bf23b1c33bfc0c0317e1b838d23aa97a67fe496
ananya <ananyay.work@gmail.com>
2018-11-05 10:29:34 -0600

    adding code and js file for busy airports

--- app.py
+++ app.py
@@ -19,29 +19,51 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/flights.sqlite"
-db = SQLAlchemy(app)
+engine = create_engine("sqlite:///db/flights_data.sqlite", encoding='utf8')
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
 
+session = Session(engine)
+flight_data = pd.read_sql("SELECT * FROM flights_data",engine)
 # Save references to each table
 # Samples_Metadata = Base.classes.sample_metadata
 # Samples = Base.classes.samples
 
 
+
+
 @app.route("/")
 def index():
     """Return the homepage."""
+  
     return render_template("index.html")
 
 
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
+    
 
+
+# @app.route("/top_airlines")
+# def names():
+#     flight_data = pd.read_sql("SELECT * FROM flights_data",engine)
+#     flight_data_carrier =flight_data.groupby(['carrier_name'])
+#     top_airlines = flight_data_carrier['arr_del15'].mean()
+#     top_airlines = top_airlines.sort_values(ascending = False)
+#     topten = top_airlines.head(10)
+#     top = topten.to_dict()
+   
+#     return (top)
+@app.route("/top_airports")
+def airports():
+    airports_data = pd.read_sql("SELECT airport_name,arr_flights FROM flights_data",engine)
+    airport_grouped = airports_data.groupby(["airport_name"])
+    top_airports = airport_grouped['arr_flights'].sum()
+    top_airports = top_airports.sort_values(ascending = False)
+    topten_airports = top_airports.head(10)
+    top_airport_names = topten_airports.to_dict()
+    return jsonify(top_airport_names)
 
 
 if __name__ == "__main__":
--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,17 @@
 function buildMetadata(sample) {
-
-}
-
-function buildCharts(sample) {
-};
-
-
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
-
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
-
-// Initialize the dashboard
-init();
+    // var airlineplot = d3.select("#graph1")
+    d3.json("/top_airports").then(successHandle).catch(errorHandle)
+    function successHandle(data){
+      console.log(data)
+      var trace = [{
+        x :data.airport_name,
+        y :data.arr_flights,
+        type :'bar'
+      }]
+      Plotly.newPlot("graph1",trace) 
+    }     
+    function errorHandle(error){
+      console.log(error)
+    } 
+  }
+buildMetadata()
