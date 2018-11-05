bed603da885a3aba4a8f74513c6dc9988befcdf4
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-11-05 11:04:08 -0600

    consolidating code, rough front end
    - Drop down not functioning

--- Flight-Statistics-Dashboard/static/js/app.js
+++ Flight-Statistics-Dashboard/static/js/app.js
@@ -1,24 +1,50 @@
-
-
 function buildMetadata(sample) {
 
 }
 
 function buildCharts(sample) {
-};
 
+  //Summary Chart
+
+  // Map
+  
+//    // var airlineplot = d3.select("#graph1")
+//    d3.json("/top_airports").then(successHandle).catch(errorHandle)
+//    function successHandle(data){
+//      console.log(data)
+//      var trace = [{
+//        x :data.airport_name,
+//        y :data.arr_flights,
+//        type :'bar'
+//      }]
+//      Plotly.newPlot("graph1",trace) 
+//    }     
+//    function errorHandle(error){
+//      console.log(error)
+//    } 
+//  }
+
+  // Pie Chart 
+
+  // Bar 
+
+
+
+}
 
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = d3.select("#selYear");
 
   // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
+  d3.json("/years").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
+        conosle.log(sampleNames)
+    
     });
 
     // Use the first sample from the list to build the initial plots
@@ -36,5 +62,3 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
-
-
