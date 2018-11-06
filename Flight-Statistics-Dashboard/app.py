51ac864216755dbabb27f81987863641dbb74d18
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-11-05 20:37:20 -0600

    Updating template/db files

--- Flight-Statistics-Dashboard/app.py
+++ Flight-Statistics-Dashboard/app.py
@@ -1,16 +1,15 @@
 # Import Dependencies
 import os
-
 import pandas as pd
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,inspect
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+#import csv,sqlite
 
 app = Flask(__name__)
 
@@ -19,7 +18,9 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/flights.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/flights_data.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -32,16 +33,47 @@ Base.prepare(db.engine, reflect=True)
 # Samples = Base.classes.samples
 
 
+
+engine = create_engine("sqlite:///db/flights_data.sqlite", encoding='utf8')
+conn = engine.connect()
+session = Session(engine)
+
+
+
+
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
+@app.route("/years")
+def years():
+    years = pd.read_sql("SELECT year FROM flights_data",engine)
+    year_list = years['year'].unique()
+    return jsonify(year_list.tolist())
 
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
 
+# @app.route("/topflights2018")
+# def topflights2018():
+#     """Return a list of sample names with their average delay time."""
+#     flight_data = pd.read_sql("SELECT * FROM flights_data",engine)
+#     flight_data_delay = flight_data[[ "year", "carrier_name", "arr_delay"]]
+#     flight_data_delay_carrier = flight_data_delay.loc[(flight_data_delay['year'] == 2018), :]
+#     flight_data_delay_grouped = flight_data_delay_carrier.groupby(['carrier_name'])
+#     top_flights = flight_data_delay_grouped["arr_delay"].mean()
+#     flights_2018 = top_flights.to_dict()
+#     return jsonify(flights_2018)
+
+    
+@app.route("/top_airports")
+def airports():
+    airports_data = pd.read_sql("SELECT airport_name,arr_flights FROM flights_data",engine)
+    airport_grouped = airports_data.groupby(["airport_name"])
+    top_airports = airport_grouped['arr_flights'].sum()
+    top_airports = top_airports.sort_values(ascending = False)
+    topten_airports = top_airports.head(10)
+    top_airport_names = topten_airports.to_dict()
+    return jsonify(top_airport_names)
 
 
 if __name__ == "__main__":
--- Flight-Statistics-Dashboard/static/js/app.js
+++ Flight-Statistics-Dashboard/static/js/app.js
@@ -1,36 +1,57 @@
-function buildMetadata(sample) {
+function buildMetadata(flight) {
 
 }
 
-function buildCharts(sample) {
-};
+function buildCharts(flight) {
+
+  //Summary Chart
+
+  // Map
+  
+//    // var airlineplot = d3.select("#graph1")
+//    d3.json("/top_airports").then(successHandle).catch(errorHandle)
+//    function successHandle(data){
+//      console.log(data)
+//      var trace = [{
+//        x :data.airport_name,
+//        y :data.arr_flights,
+//        type :'bar'
+//      }]
+//      Plotly.newPlot("graph1",trace) 
+//    }     
+//    function errorHandle(error){
+//      console.log(error)
+//    } 
+//  }
+
+  // Pie Chart 
+
+  // Bar 
 
 
+
+}
+console.log()
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = d3.select("#selYear");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
+  // Use the list of flight names to populate the select options
+  d3.json("/years").then((flightYears) => {
+    flightYears.forEach((year) => {
       selector
         .append("option")
-        .text(sample)
-        .property("value", sample);
+        .text(year)
+        .property("value", year);
     });
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
+    // Use the first flight from the list to build the initial plots
+    const firstflight = flightYears[0];
+    buildCharts(firstflight);
+    buildMetadata(firstflight);
   });
 }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
 
 // Initialize the dashboard
 init();
