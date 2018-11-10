d83d8cb0b76b288a9fac53dab16302fee784d60d
ananya <ananyay.work@gmail.com>
2018-11-10 15:22:55 -0600

    adding code for circle reset

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ d3.selectAll(".month").on("click", function(){
 })
 
 function markerSize(total_flights) {
-    return total_flights;
+    return total_flights/2;
 }
 
 var maxBounds = [
@@ -27,47 +27,49 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
 }).addTo(myMap)
 
+let circles
 
 function buildCharts(month_value) {
     console.log(`in 2 :`,month_value)
     d3.json(`/monthly_count/${month_value}`).then(successHandle).catch(errorHandle)
-    
+
     function successHandle(response){
-        console.log(response)
+        //console.log(response)
+
         for (var resp in response){
             lat_lng = []
             lat_lng.push(response[resp].Latitude)
             lat_lng.push(-response[resp].Longitude)
             total_flights = response[resp].sum_arr_flights
             airport_name = resp
-            console.log(lat_lng)
+            //console.log(lat_lng)
             console.log(total_flights)
-            console.log(airport_name)
-            L.circle(lat_lng, {
-            stroke: false,
-            fillOpacity: 0.75,
-            color: "white",
-            fillColor: "purple",
-            radius: markerSize(total_flights)
-            })
-            .bindPopup("<h6>" + airport_name + "</h6> <hr> <h6>city: " + airport_name + "</h6>" +"</h4> <hr> <h6>Total flights: " + total_flights + "</h6>")
-            .addTo(myMap);
-        }
+            //console.log(airport_name)
+            circles = L.circle(lat_lng, {
+                stroke: false,
+                fillOpacity: 0.75,
+                color: "white",
+                fillColor: "purple",
+                radius: markerSize(total_flights)
+                })
+                .bindPopup("<h6>" + airport_name + "</h6> <hr> <h6>city: " + airport_name + "</h6>" +"</h4> <hr> <h6>Total flights: " + total_flights + "</h6>")
+                .addTo(myMap);
+            }
     }
     function errorHandle(error){
         console.log(`error is :`,{error})
-    }   
-}
-// buildCharts()  
+    }
 
-function init(){
-    buildCharts(1)
 }
-init()
+// buildCharts()
+function clearCircles(){
+    d3.selectAll('g').html("")
+  }
+  
 function someClick(month){
-    console.log(month)
+    console.log(circles)
+    clearCircles()
     buildCharts(month)
     // $(this).addClass('active')
-}    
-
+}
 
